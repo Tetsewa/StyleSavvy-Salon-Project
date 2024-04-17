68086e878e32aa949c55c62dbbab7774ebcbd86a
Tetsewa <amatwilliams@gmail.com>
2024-04-17 05:11:25 +0200

    create all page files and navbar component

---
+++ src/pages/MakeAnAppointment.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MakeAnAppointment() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default MakeAnAppointment
---
+++ src/pages/MyReservation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MyReservation() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default MyReservation
