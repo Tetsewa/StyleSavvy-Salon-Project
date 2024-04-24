6d606db75905a410a42ecc44595aad5f7f26f499
Sowjanyakambhampati <93071280+Sowjanyakambhampati@users.noreply.github.com>
2024-04-24 23:51:30 +0200

    Update Team.jsx

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -8,58 +8,42 @@ const Team = () => {
             <span>
           <h1>Our Team</h1>
         </span>
-            <div className=" tracking-[0.02em] capitalize font-alegreya  ">
-                <h1>Home/Team</h1>
-            </div>
-            <div className="space-x-20 bg-oldlace">
-                <div className="inline-block " >
+            {/*<div className=" tracking-[0.02em] capitalize font-alegreya  ">*/}
+            {/*    <h1>Home/Team</h1>*/}
+            {/*</div>*/}
 
-                    <img
-                        className=" object-contain"
-                        alt=""
-                        src="/team2.png"
-                    />
-                    <div className="rounded-t-none rounded-b-3xs bg-oldlace " />
-                    <div className=" justify-center items-center tracking-[0.02em]  capitalize font-semibold  ">
+            <div className="control-row">
+                <div className="control">
+                    <img className=" object-contain" alt="" src="/team2.png"/>
+                    <div className="capitalize font-semibold text-center">
                         Sarah watson
                     </div>
-                    <div className=" text-base tracking-[0.02em]  capitalize font-medium font-roboto inline-block ">Nail & Skin Care specialist</div>
-                </div>
-
-
-                <div className="inline-block w-[218px] h-[186px]" >
-                    <img
-                        className=" object-cover"
-                        alt=""
-                        src="/team1.png"
-                    />
-                    <div >
-                        <div className=" rounded-t-none rounded-b-3xs bg-oldlace " />
-                        <div className=" tracking-[0.02em]  capitalize font-semibold inline-block ">
-                            Jack Wilson
-                        </div>
-                        <div className=" text-base tracking-[0.02em]  capitalize font-medium font-roboto inline-block ">haircut & Massage specialist</div>
+                    <div className=" text-base tracking-[0.02em]  capitalize font-medium font-roboto text-center">Nail
+                        & Skin Care specialist
                     </div>
                 </div>
-                <div className="inline-block w-[218px] h-[186px]">
-                    <img
-                        className="object-cover"
-                        alt=""
-                        src="/team3.png"
-                    />
-                    <div>
-                        {/* <div className=" rounded-t-none rounded-b-3xs bg-oldlace " /> */}
-                        <div>
-                            <div className=" tracking-[0.02em]  capitalize font-semibold inline-block ">
-                                <b>David Barnes</b>
-                            </div>
-                        </div>
-                        <div className=" capitalize font-medium font-roboto inline-block ">
-                            haircut specialist
-                        </div>
+                <div className="control">
+                    <img className=" object-contain" alt="" src="/team1.png"/>
+                    <div className="capitalize font-semibold text-center">
+                        Jack Wilson
+                    </div>
+                    <div
+                        className=" text-base tracking-[0.02em]  capitalize font-medium font-roboto text-center">Haircut
+                        & Massage Specialist
+                    </div>
+                </div>
+                <div className="control">
+                    <img className=" object-contain" alt="" src="/team3.png"/>
+                    <div className="capitalize font-semibold text-center">
+                        David Barnes
+                    </div>
+                    <div
+                        className=" text-base tracking-[0.02em]  capitalize font-medium font-roboto text-center">Haircut
+                        Specialist
                     </div>
                 </div>
             </div>
+
         </div>
     );
 };
