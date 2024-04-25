82888b29bd8789781f26164ee121384b4b02e7fb
Sowjanyakambhampati <93071280+Sowjanyakambhampati@users.noreply.github.com>
2024-04-25 13:00:35 +0200

    Update MyReservation.jsx

--- src/pages/MyReservation.jsx
+++ src/pages/MyReservation.jsx
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
-import { InputText } from 'primereact/inputtext';
-import { InputNumber } from 'primereact/inputnumber';
-import { Dropdown } from 'primereact/dropdown';
-import { Tag } from 'primereact/tag';
-import { ProductService } from '/src/components/ProductService';
+import React, {useEffect, useState} from 'react';
+import {DataTable} from 'primereact/datatable';
+import {Column} from 'primereact/column';
+import {InputText} from 'primereact/inputtext';
+import {InputNumber} from 'primereact/inputnumber';
+import {Dropdown} from 'primereact/dropdown';
+import {Tag} from 'primereact/tag';
+import {ProductService} from '/src/components/ProductService';
+import HomeBanner from "../components/HomeBanner";
 
 const MyReservation = () => {
     const [products, setProducts] = useState(null);
@@ -33,7 +34,7 @@ const MyReservation = () => {
 
     const onRowEditComplete = (e) => {
         let _products = [...products];
-        let { newData, index } = e;
+        let {newData, index} = e;
 
         _products[index] = newData;
 
@@ -41,7 +42,7 @@ const MyReservation = () => {
     };
 
     const textEditor = (options) => {
-        return <InputText type="text" value={options.value} onChange={(e) => options.editorCallback(e.target.value)} />;
+        return <InputText type="text" value={options.value} onChange={(e) => options.editorCallback(e.target.value)}/>;
     };
 
     const statusEditor = (options) => {
@@ -59,7 +60,8 @@ const MyReservation = () => {
     };
 
     const priceEditor = (options) => {
-        return <InputNumber value={options.value} onValueChange={(e) => options.editorCallback(e.value)} mode="currency" currency="USD" locale="en-US" />;
+        return <InputNumber value={options.value} onValueChange={(e) => options.editorCallback(e.value)} mode="currency"
+                            currency="USD" locale="en-US"/>;
     };
 
     const statusBodyTemplate = (rowData) => {
@@ -67,7 +69,7 @@ const MyReservation = () => {
     };
 
     const priceBodyTemplate = (rowData) => {
-        return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(rowData.price);
+        return new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(rowData.price);
     };
 
     const allowEdit = (rowData) => {
@@ -75,14 +77,54 @@ const MyReservation = () => {
     };
 
     return (
-        <div className="card p-fluid">
-            <DataTable value={products} editMode="row" dataKey="id" onRowEditComplete={onRowEditComplete} tableStyle={{ minWidth: '50rem' }}>
-                <Column field="code" header="Code" editor={(options) => textEditor(options)} style={{ width: '20%' }}></Column>
-                <Column field="name" header="Name" editor={(options) => textEditor(options)} style={{ width: '20%' }}></Column>
-                <Column field="inventoryStatus" header="Status" body={statusBodyTemplate} editor={(options) => statusEditor(options)} style={{ width: '20%' }}></Column>
-                <Column field="price" header="Price" body={priceBodyTemplate} editor={(options) => priceEditor(options)} style={{ width: '20%' }}></Column>
-                <Column rowEditor={allowEdit} headerStyle={{ width: '10%', minWidth: '8rem' }} bodyStyle={{ textAlign: 'center' }}></Column>
-            </DataTable>
+
+        <div>
+            <HomeBanner/>
+
+            <form style={{width: '80%'}}>
+
+                <h2>Finalise your booking!</h2>
+                <div className="control-row">
+                    <div className="control">
+                        <label htmlFor="first-name">First Name</label>
+                        <input type="text" id="first-name" name="first-name"/>
+                    </div>
+
+                    <div className="control">
+                        <label htmlFor="last-name">Last Name</label>
+                        <input type="text" id="last-name" name="last-name"/>
+                    </div>
+                </div>
+
+                <div className="control">
+                    <label htmlFor="email">Email</label>
+                    <input id="email" type="email" name="email"/>
+                </div>
+
+                <div className="control">
+                    <label htmlFor="contact-number">Contact Number</label>
+                    <input type="text" id="contact-number" name="contact-number"/>
+                </div>
+
+                <hr/>
+                <div className="card p-fluid">
+                    <DataTable value={products} editMode="row" dataKey="id" onRowEditComplete={onRowEditComplete}
+                               tableStyle={{minWidth: '50rem'}}>
+                        <Column field="code" header="Services" editor={(options) => textEditor(options)}
+                                style={{width: '20%'}}></Column>
+                        <Column field="name" header="Name" editor={(options) => textEditor(options)}
+                                style={{width: '20%'}}></Column>
+                        <Column field="inventoryStatus" header="Status" body={statusBodyTemplate}
+                                editor={(options) => statusEditor(options)} style={{width: '20%'}}></Column>
+                        <Column field="price" header="Price" body={priceBodyTemplate}
+                                editor={(options) => priceEditor(options)} style={{width: '20%'}}></Column>
+                        <Column rowEditor={allowEdit} headerStyle={{width: '10%', minWidth: '8rem'}}
+                                bodyStyle={{textAlign: 'center'}}></Column>
+                    </DataTable>
+                </div>
+            </form>
+
+
         </div>
     );
 }
