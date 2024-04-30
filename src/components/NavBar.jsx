393e75bc54877162fde82c4311a6e8584bcaab4f
Tetsewa <amatwilliams@gmail.com>
2024-04-30 19:52:07 +0200

    added styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,20 +6,20 @@ const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-      <nav >
+      <nav className="h-28">
         <div className="flex justify-between px-3">
           <img
               alt="logo"
               src="/stylesavvy-logo-png-1@2x.png"
               width={140} height={170}
           />
-          <div className="flex gap-5 justify-center items-center hidden md:flex">
-            <Link to={`/`} className="text-black">Home</Link>
-            <Link to={`/AboutUs`} className="text-black">About Us</Link>
-            <Link to={`/MakeAnAppointment`} className="text-black">Make An Appointment</Link>
-            <Link to={`/Services`} className="text-black">Services/Prices</Link>
-            <Link to={`/Team`} className="text-black">Team</Link>
-            <Link to={`/MyReservation`} className="text-black">My Reservation</Link>
+          <div className="flex gap-5 justify-center items-center hidden md:flex text-xl">
+            <Link to={`/`} className="text-black no-underline">Home</Link>
+            <Link to={`/AboutUs`} className="text-black no-underline no-underline">About Us</Link>
+            <Link to={`/MakeAnAppointment`} className="text-black no-underline">Make An Appointment</Link>
+            <Link to={`/Services`} className="text-black no-underline">Services/Prices</Link>
+            <Link to={`/Team`} className="text-black no-underline">Team</Link>
+            
 
 
           </div>
@@ -47,9 +47,6 @@ const Navbar = () => {
             </svg>
           </button>
 
-          {/* <div className="hidden md:flex items-center font-semibold mr-4">
-            <button className=" bg-black text-white p-4 rounded">My Reservation</button>
-          </div> */}
         </div>
 
 
@@ -57,19 +54,18 @@ const Navbar = () => {
         {menuOpen && (
             <div >
               <div className="flex flex-col items-center justify-center gap-5 ">
-                <Link to={`/`} className="text-black">Home</Link>
-                <Link to={`/About`} className="text-black">About Us</Link>
-                <Link to={`/MakeAnAppointment`} className="text-black">Make An Appointment</Link>
-                <Link to={`/Services`} className="text-black">Services/Prices</Link>
-
-                <Link to={`/Team`} className="text-black">Team</Link>
+                <Link to={`/`} className="text-black no-underline">Home</Link>
+                <Link to={`/About`} className="text-black no-underline">About Us</Link>
+                <Link to={`/MakeAnAppointment`} className="text-black no-underline">Make An Appointment</Link>
+                <Link to={`/Services`} className="text-black no-underline">Services/Prices</Link>
+                <Link to={`/Team`} className="text-black no-underline">Team</Link>
 
 
 
               </div>
 
               {/* <div className="flex items-center justify-center font-semibold mt-5">
-                <button className=" bg-black text-white p-4 rounded"><Link to="/MyReservation" className="text-black">MyReservation</Link> </button>
+                <button className=" bg-black text-white p-4 rounded"><Link to="/MyReservation" className="text-black no-underline">MyReservation</Link> </button>
               </div> */}
 
             </div>
@@ -99,12 +95,12 @@ export default Navbar;
 //           />
 //         </Link>
 //         <div className="hidden md:flex gap-5">
-//           <Link to={`/`} className="text-black">Home</Link>
-//           <Link to={`/AboutUs`} className="text-black">About Us</Link>
-//           <Link to={`/MakeAnAppointment`} className="text-black">Make An Appointment</Link>
-//           <Link to={`/Services`} className="text-black">Services/Prices</Link>
-//           <Link to={`/Team`} className="text-black">Team</Link>
-//           <Link to={`/MyReservation`} className="text-black">My Reservation</Link>
+//           <Link to={`/`} className="text-black no-underline">Home</Link>
+//           <Link to={`/AboutUs`} className="text-black no-underline">About Us</Link>
+//           <Link to={`/MakeAnAppointment`} className="text-black no-underline">Make An Appointment</Link>
+//           <Link to={`/Services`} className="text-black no-underline">Services/Prices</Link>
+//           <Link to={`/Team`} className="text-black no-underline">Team</Link>
+//           <Link to={`/MyReservation`} className="text-black no-underline">My Reservation</Link>
 //         </div>
 //         <button onClick={() => setMenuOpen((prev) => !prev)} className="md:hidden">
 //           <svg
@@ -130,12 +126,12 @@ export default Navbar;
 //       {menuOpen && (
 //         <div className="bg-white">
 //           <div className="container mx-auto py-2 md:hidden flex flex-col gap-2">
-//             <Link to={`/`} className="text-black">Home</Link>
-//             <Link to={`/About`} className="text-black">About Us</Link>
-//             <Link to={`/MakeAnAppointment`} className="text-black">Make An Appointment</Link>
-//             <Link to={`/Services`} className="text-black">Services/Prices</Link>
-//             <Link to={`/Team`} className="text-black">Team</Link>
-//             <Link to={`/MyReservation`} className="text-black">My Reservation</Link>
+//             <Link to={`/`} className="text-black no-underline">Home</Link>
+//             <Link to={`/About`} className="text-black no-underline">About Us</Link>
+//             <Link to={`/MakeAnAppointment`} className="text-black no-underline">Make An Appointment</Link>
+//             <Link to={`/Services`} className="text-black no-underline">Services/Prices</Link>
+//             <Link to={`/Team`} className="text-black no-underline">Team</Link>
+//             <Link to={`/MyReservation`} className="text-black no-underline">My Reservation</Link>
 //           </div>
 //         </div>
 //       )}
