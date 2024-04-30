e760759e70421623ece80b32d6790126ac2b200b
Sowjanyakambhampati <93071280+Sowjanyakambhampati@users.noreply.github.com>
2024-04-30 16:25:00 +0200

    Update Services.jsx

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,24 +1,13 @@
-
-
-
 import React from 'react';
 import HomeBanner from '../components/HomeBanner';
 import PriceTable from '../components/PriceTable';
 
-
 function Services() {
-  //   const [products, setProducts] = useState([]);
-  //   useEffect(() => {
-  //     ProductService.getProductsMini().then(data => setProducts(data));
-  // }, []);
-
   return (
     <div>
-
       <HomeBanner />
       <PriceTable />
     </div>
   );
 }
-
 export default Services;
